1979acdb9d0c9082cb64330d759c69f7a95affae
binarykittenDE <franziska.haaf@gmail.com>
2018-01-15 12:41:24 +0100

    Changed the title

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            hello: 'Welche Kulturangebote Münchens Touristen nutzen'
+            hello: 'Die Nutzung Münchens Kulturangebote - jetzt mit Bildern!'
         };
     }
 
